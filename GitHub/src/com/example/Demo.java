edfcff404f7732a015c79e10d912bb7cbea8faa8
shivkumar3214github <shivakumarchandapure1@gmail.com>
2022-11-14 18:48:50 +0530

    my second commit

--- GitHub/src/com/example/Demo.java
+++ GitHub/src/com/example/Demo.java
@@ -1,9 +1,24 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList al =new ArrayList ();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		
+		System.out.println(al);
+		
+Iterator itr	=al.iterator();
+ while(itr.hasNext()) {
+	 System.out.println(itr.next());
+ }
+		
 
 	}
 
